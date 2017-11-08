6b2f9feabee1c1086c236e412c1973fb1e451f84
Matttd <Hosperis@wp.pl>
2017-11-09 00:19:57 +0100

    Add Firebase notyfications fix

--- app/src/main/java/pl/edu/uwb/mobiuwb/firebasenotyfication/UwbFirebaseInstanceIDService.java
+++ app/src/main/java/pl/edu/uwb/mobiuwb/firebasenotyfication/UwbFirebaseInstanceIDService.java
@@ -4,7 +4,7 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
 /**
- *  Klasa odpowiadająca za otrzymanie tokenu przy rejestracji aplikacji.
+ *  Klasa odpowiadająca za otrzymanie tokenu przy rejestracji aplikacji. Tymczasowo nigdzie nie używana.
  */
 
 public class UwbFirebaseInstanceIDService extends FirebaseInstanceIdService {
